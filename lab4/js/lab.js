24d943e34e0abde78a618540880d0fdb6ff50786
Bruce Bai <brbai@ucsc.edu>
2023-04-24 18:00:36 -0700

    stylistic and lab updates

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,15 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Implement Connect Four Game
+// Author: Bruce Bai <brbai@ucsc.edu>
+// Date: April 23, 2023
+
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+// this is an example function and this comment tells what it does and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+
+//Print board before each turn
+//Accept user input
+//Validate User input
+    //Check that column still has empty slots
+    //Place piece on lowest empty row of column
+//Evaluate possible moves
+    //Check which columns still are empty
+    //Prioritize moves where user might come close to winning
+//Choose computer move
+//Test for win or draw situation
+    //Win: four in a row, horizontally, vertically, or diagonally
+    //draw : board filled up with no four in a row for either player
+
 }
 
 function main() {
