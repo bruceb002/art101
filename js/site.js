e8f80e77e190d3273dcead89ae619eb7311ec44e
Bruce Bai <brbai@ucsc.edu>
2023-04-25 12:17:39 -0700

    seperated js from html and other big changes

--- js/site.js
+++ js/site.js
@@ -2,19 +2,36 @@
 // Author: Your Name
 // Date:
 
-// Constants
-
+// Constants/variables
+let slideIndex = 0;
+let i = 0;
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function myFunction() {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }    
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(myFunction, 3000); // Change image every 3 seconds
 }
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  myFunction();
 }
 
 // let's get this party started
