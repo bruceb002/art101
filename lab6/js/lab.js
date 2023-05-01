193fbfc77b4b5e57f7f36b8a0d2257fa17b2784b
Bruce Bai <brbai@ucsc.edu>
2023-04-30 18:47:05 -0700

    done with lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,24 +1,36 @@
-// index.js - purpose and description here
+// index.js - Introduction to objects and functions
 // Author: Bruce Bai
 // Date: April 27, 2023
 
 // Constants/Variables
 myTransport = ["car", "bus"];
 
-//was difficult to assign value to age
-//solution: initialize age to null then reset value later
 const myMainRide = {
   model: "Lamborghini",
   make: "Urus",
   color: "red",
   year: 2017,
-  age: null
+  age: function() {
+    return 2023 - this.year;
+  }
 };
 
-myMainRide.age = 2023 - myMainRide.year;
+//Credit: ChatGPT
+//Reasoning: 
+//JSON.stringify() might serialize age's numerical value unpredictably,
+//so we should explicitly convert the age to a string so it's predictable
+const replacer = function(key, value) {
+  if (key === "age") {
+    return myMainRide.age().toString();
+  } else {
+    return value;
+  }
+};
 
 //output 
-document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\n'), "</pre>");
+//Needed to replace 'null' with 'replacer' to specify the serialization of all the values
+//(which would be the same as initially for every other property other than age)
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, replacer, '\n'), "</pre>");
 
 // Functions
 
