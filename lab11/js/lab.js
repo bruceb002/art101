fdb5dabbda6fea5ba7ee6da6a81e09f2045b7a8d
Bruce Bai <brbai@ucsc.edu>
2023-05-16 09:17:20 -0700

    added arrows

---
+++ lab11/js/lab.js
@@ -0,0 +1,13 @@
+// index.js - Learning how to process a sumbission form with a button
+// Author: Bruce Bai
+// Date: May 14, 2023
+
+//Functions
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+  // let's get this party started
+  main();
