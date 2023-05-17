57da61213e8a96a2fd239e02fc9642fe9fe64e06
Bruce Bai <brbai@ucsc.edu>
2023-05-17 15:34:50 -0700

    done with 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,9 +3,33 @@
 // Date: May 14, 2023
 
 //Functions
+function create_buttons() {
+  $("h2").append("<br>");
+  $("h2").append("<button>");
+  $("button").html("Press");
+
+  $("#one button").click(() => {
+    $("#one .special").toggle();
+  }); 
+
+  $("#two button").click(() => {
+    $("#two .special").toggle();
+  }); 
+
+  $("#three button").click(() => {
+    $("#three .special").toggle();
+  }); 
+
+  $("#four button").click(() => {
+    $("#four .special").toggle();
+  }); 
+
+}
+
 
 function main() {
   console.log("Main function started.");
+  $(document).ready(create_buttons);
   // the code that makes everything happen
 }
 
