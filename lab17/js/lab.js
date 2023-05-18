fd166a80abd0154df9db769d777781c793613b3a
Bruce Bai <brbai@ucsc.edu>
2023-05-18 14:28:29 -0700

    full arrows

---
+++ lab17/js/lab.js
@@ -0,0 +1,12 @@
+// index.js - 
+// Author: Bruce Bai
+// Date: 
+
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+  // let's get this party started
+  main();
