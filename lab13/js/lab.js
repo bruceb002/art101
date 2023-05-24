8eead0919edc9195ffc3950d5a9139a77db601d1
Bruce Bai <brbai@ucsc.edu>
2023-05-24 14:48:47 -0700

    done with lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,11 +1,32 @@
-// index.js - 
+// index.js - Playing around with loops
 // Author: Bruce Bai
-// Date: 
+// Date: May 24, 2023
+
+function FizzBuzzBoom() {
+  var res = "";
+
+  for(var i = 1; i <= 200; i++) {
+    res += i.toString() + ": ";
+    if(i % 3 == 0) {
+      res += "Fizz";
+    }
+    if(i % 5 == 0) {
+      res += "Buzz";
+    }
+    if(i % 7 == 0) {
+      res += "Boom";
+    }
+    res += "<br>";
+  }
+  $("#output").html(res); // fixed
+
+}
 
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  FizzBuzzBoom();
 }
 
   // let's get this party started
