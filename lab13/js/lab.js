8d784a4015dd31a08f1797f3ace8851bfcd54bad
Bruce Bai <brbai@ucsc.edu>
2023-05-24 15:46:40 -0700

    finished lab13 ec

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -2,19 +2,30 @@
 // Author: Bruce Bai
 // Date: May 24, 2023
 
-function FizzBuzzBoom() {
+function FizzBuzzBoom(maxNum, factors, factor_outs) {
   var res = "";
+  var l = factors.length;
+  if(l != factor_outs.length || maxNum == "") {
+    $("#output").html("You did not fill in one or more of the forms!");
+    return;
+  }
 
-  for(var i = 1; i <= 200; i++) {
+  
+
+  for(var i = 1; i <= maxNum; i++) {
     res += i.toString() + ": ";
-    if(i % 3 == 0) {
-      res += "Fizz";
-    }
-    if(i % 5 == 0) {
-      res += "Buzz";
-    }
-    if(i % 7 == 0) {
-      res += "Boom";
+    for(var j = 0; j < l; j++) {
+      if(factors[j] == "") {
+        $("#output").html("You did not fill in one or more of the forms!");
+        return;
+      }
+      if(i % factors[j] == 0) {
+        if(factor_outs[j] == "") {
+          $("#output").html("You did not fill in one or more of the forms!");
+          return;
+        }
+        res += factor_outs[j];
+      }
     }
     res += "<br>";
   }
@@ -26,7 +37,24 @@ function FizzBuzzBoom() {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
-  FizzBuzzBoom();
+  var start = document.getElementById("submission");
+  start.addEventListener("click", () => {
+    var m = $("#maxNum").val();
+
+    var factors = document.getElementsByClassName("factor");
+    f = [];
+    for(var i = 0; i < factors.length; i++) {
+      f.push(factors[i].value);
+    }
+
+    f_o = [];
+    var phrases = document.getElementsByClassName("phrase");
+    for(var i = 0; i < factors.length; i++) {
+      f_o.push(phrases[i].value);
+    }
+
+    FizzBuzzBoom(m, f, f_o);
+  });
 }
 
   // let's get this party started
