6ddbb184619934718ecaa0530455b1a0d875d100
Bruce Bai <brbai@ucsc.edu>
2023-06-05 16:26:44 -0700

    lets try

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,11 +1,30 @@
-// index.js - 
+// index.js - Get to discover API's with JSON formatting. Also fixing CORS issue once and for all.
 // Author: Bruce Bai
-// Date: 
+// Date: June 5, 2023
+
+function get_comic_data() {
+  $.ajax({
+    url: 'https://xkcd.com/info.0.json',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      var comicObj = data;
+      
+      // Process the data and add it to your webpage
+      $('#output').append('<h1>' + comicObj.title + '</h1>');
+      $('#output').append('<img src="' + comicObj.img + '" alt="' + comicObj.alt + '" title="' + comicObj.alt + '">');
+    },
+    error: function(xhr, status, error) {
+      console.error('Error:', error);
+    }
+  });
+}
 
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  get_comic_data();
 }
 
   // let's get this party started
