942df7c190b838422b887bfabc0f76c94987ea31
Bruce Bai <brbai@ucsc.edu>
2023-06-14 19:57:01 -0700

    hmm

--- experiments/index.js
+++ experiments/index.js
@@ -1,8 +1,8 @@
 function main() {
     document.addEventListener('DOMContentLoaded', function() {
         $("#myButton").click(() => {
+            console.log("Let's see what the section is: ", JSON.stringify($("#download_here")));
             html2canvas($("#download_here")).then(canvas => {
-                console.log("Let's see what canvas is: ", JSON.stringify(canvas));
                 theCanvas = canvas;
                 $("section").appendChild(canvas);
                 canvas.toBlob(function (blob) {
