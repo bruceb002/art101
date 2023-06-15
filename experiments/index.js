d07c557ff1bc4e2509e193795024e761ff792da5
Bruce Bai <brbai@ucsc.edu>
2023-06-14 19:42:12 -0700

    deleted check msgs

--- experiments/index.js
+++ experiments/index.js
@@ -3,7 +3,6 @@ function main() {
         $("#myButton").click(() => {
             html2canvas($("#download_here"), {
                 onrendered: function(canvas) {
-                    console.log("It also made it here!");
                     console.log("Let's see what canvas is: ", JSON.stringify(canvas));
                     theCanvas = canvas;
                     $("section").appendChild(canvas);
