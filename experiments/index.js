ae9c02663950f3a89398d00a2847e5cd54d353de
Bruce Bai <brbai@ucsc.edu>
2023-06-14 20:18:41 -0700

    wait also this change

--- experiments/index.js
+++ experiments/index.js
@@ -1,6 +1,5 @@
 function main() {
     $("#myButton").click(() => {
-        console.log("Let's see what the section is: ", JSON.stringify($("#download_here")));
         html2canvas(document.getElementById("download_here")).then(canvas => {
             theCanvas = canvas;
             $("section").appendChild(canvas);
