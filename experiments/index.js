5dfb9f553dcb09354f669b7fd40987126939ec54
Bruce Bai <brbai@ucsc.edu>
2023-06-14 22:37:48 -0700

    whats rlly going on

--- experiments/index.js
+++ experiments/index.js
@@ -1,5 +1,6 @@
 function main() {
     $("#myButton").click(() => {
+        console.log("This is the section: " + JSON.stringify(document.getElementById("download_here")))
         html2canvas(document.getElementById("download_here")).then(canvas => {
             theCanvas = canvas;
             canvas.toBlob(function (blob) {
