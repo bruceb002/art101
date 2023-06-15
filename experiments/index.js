f383001695b6956fbc6bf79e06a4e735c8bd82a5
Bruce Bai <brbai@ucsc.edu>
2023-06-14 19:31:01 -0700

    updated method

---
+++ experiments/index.js
@@ -0,0 +1,19 @@
+function main() {
+    console.log("Wth, is it even here?");
+    $("#myButton").click(() => {
+        console.log("It made it here!");
+        html2canvas($("#download_here"), {
+            onrendered: function(canvas) {
+                console.log("It also made it here!");
+                console.log("Let's see what canvas is: ", JSON.stringify(canvas));
+                theCanvas = canvas;
+                $("section").appendChild(canvas);
+                canvas.toBlob(function (blob) {
+                    saveAs(blob, "Dashboard.png");
+                });
+            }
+        });
+    });
+}
+
+main();
