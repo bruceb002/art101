ccbe54a56168594fbc7c51f4ac37076d9d5892a2
Bruce Bai <brbai@ucsc.edu>
2023-06-14 19:53:09 -0700

    Testingg"

--- experiments/index.js
+++ experiments/index.js
@@ -1,16 +1,17 @@
 function main() {
     document.addEventListener('DOMContentLoaded', function() {
         $("#myButton").click(() => {
-            html2canvas($("#download_here"), {
-                onrendered: function(canvas) {
-                    console.log("Let's see what canvas is: ", JSON.stringify(canvas));
-                    theCanvas = canvas;
-                    $("section").appendChild(canvas);
-                    canvas.toBlob(function (blob) {
-                        saveAs(blob, "Dashboard.png");
-                    });
-                }
-            });
+            html2canvas($("#download_here")).then(canvas => {
+                console.log("Let's see what canvas is: ", JSON.stringify(canvas));
+                theCanvas = canvas;
+                $("section").appendChild(canvas);
+                canvas.toBlob(function (blob) {
+                    saveAs(blob, "Dashboard.png");
+                });
+            })
+            .catch(error => {
+                console.error("Error occurred during rendering:", error);
+              });
         });
     });
 }
