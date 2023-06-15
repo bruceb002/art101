6e1b4812189bd8b431f8607754422d32a5953187
Bruce Bai <brbai@ucsc.edu>
2023-06-14 18:40:31 -0700

    polished code

--- experiments/index.js
+++ experiments/index.js
@@ -2,11 +2,11 @@ import html2canvas from 'html2canvas';
 
 function main() {
     $("#myButton").click(() => {
-        html2canvas($("img"), {
+        html2canvas($("#download_here"), {
             onrendered: function(canvas) {
                 theCanvas = canvas;
                 canvas.toBlob(function (blob) {
-                    saveAs (blob, "Dashboard. png");
+                    saveAs(blob, "Dashboard.png");
                 });
             }
         });
