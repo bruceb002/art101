50114fd552140ec812bb2497a1a047d92c746330
Bruce Bai <brbai@ucsc.edu>
2023-06-14 19:01:04 -0700

    agnagn

--- experiments/index.js
+++ experiments/index.js
@@ -2,10 +2,13 @@ import html2canvas from 'html2canvas';
 
 function main() {
     $("#myButton").click(() => {
+        console.log("It made it here!");
         html2canvas($("#download_here"), {
             onrendered: function(canvas) {
+                console.log("It also made it here!");
+                console.log("Let's see what canvas is: ", JSON.stringify(canvas));
                 theCanvas = canvas;
-                document.body.appendChild(canvas);
+                $("section").appendChild(canvas);
                 canvas.toBlob(function (blob) {
                     saveAs(blob, "Dashboard.png");
                 });
